50f085b47eb12063a2490bba22d7e12bfee8ebd6
Victor Boutté <mrboutte21@gmail.com>
2021-02-03 10:11:30 -0500

    added Remote Data types

--- src/video-one-ADTs/index.tsx
+++ src/video-one-ADTs/index.tsx
@@ -22,6 +22,40 @@ interface Right<A> {
 type Either<E, A> = Left<E> | Right<A>
 //#endregion
 
+// ******* Remote Data ********
+//#region Remote Data
+type RemoteProgress = {
+  readonly loaded: number
+  readonly total: Option<number>
+}
+
+type RemoteInitial = {
+  readonly _tag: 'RemoteInitial'
+}
+
+type RemotePending = {
+  readonly _tag: 'RemotePending'
+  readonly progress: Option<RemoteProgress>
+}
+
+type RemoteFailure<E> = {
+  readonly _tag: 'RemoteFailure'
+  readonly error: E
+}
+
+type RemoteSuccess<A> = {
+  readonly _tag: 'RemoteSuccess'
+  readonly value: A
+}
+
+// reference: https://github.com/devexperts/remote-data-ts/blob/master/src/remote-data.ts#L68
+type RemoteData<E, A> =
+  | RemoteInitial
+  | RemotePending
+  | RemoteFailure<E>
+  | RemoteSuccess<A>
+//#endregion
+
 //#region Product Type example
 type Hour = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 type Period = 'AM' | 'PM'
